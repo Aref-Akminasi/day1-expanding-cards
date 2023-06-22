16a66bf2c150347f46269d97b30123063e19af33
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-06-22 12:44:31 +0200

    added comments

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const panels = document.querySelectorAll('.panel');
 
+/* Defining a click event listener with a function for each panel */
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
-    removeActiveClasses();
-    panel.classList.add('active');
+    removeActiveClasses(); //First, the class 'active' will be removed from all panels
+    panel.classList.add('active'); // Second, the class 'active' will be added to the clicked panel, that what will make it expand'
   });
 });
 
